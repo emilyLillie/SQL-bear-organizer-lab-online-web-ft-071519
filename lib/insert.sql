49cb34fc2c650a01200ddd3bf69bedc27867bb3f
Emily Lillie <emilylillie3@gmail.com>
2019-08-13 22:25:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 32, "female", "black", "loud", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 30, "male", "brown", "kind", 1);
 
